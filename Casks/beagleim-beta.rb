850d6043fdb4aa98728c53513c8872ee9a997f8a
Wojciech Kapcia <wojtek@tigase.org>
2020-03-05 19:20:20 -0300

    fix beagleim-beta versioning

--- Casks/beagleim-beta.rb
+++ Casks/beagleim-beta.rb
@@ -1,5 +1,5 @@
 cask 'beagleim-beta' do
-  version 'v4.0b80'
+  version '4.0-b80'
   sha256 '47b2922873dff615944ee2fb2eac20168f22425a82dcd2fe30e70aaabaf39993'
 
   # github.com/tigase/beagle-im was verified as official when first introduced to the cask
@@ -8,5 +8,5 @@ cask 'beagleim-beta' do
   name 'Tigase BeagleIM beta'
   homepage 'https://beagle.im/'
 
-  app "BeagleIM #{version} beta.app"
+  app "BeagleIM #{version.major_minor}.app"
 end
