601b26fa4e73af3230ce108902915a0ea8fca67a
Andrzej Wójcik <andrzej.wojcik@tigase.net>
2020-03-06 18:47:53 +0100

    Update to new version 3.7

--- Casks/beagleim.rb
+++ Casks/beagleim.rb
@@ -1,6 +1,6 @@
 cask 'beagleim' do
-  version '3.6'
-  sha256 'f8ebf241e4eb7953760dc3619393e338943bbe0139ce3b3a8a833592e911a7ef'
+  version '3.7'
+  sha256 'ed051bbdbdb549d2a45cb1e14e10b17b96eba6a6a72aa10d18873876884c3cf6'
 
   # github.com/tigase/beagle-im was verified as official when first introduced to the cask
   url "https://github.com/tigase/beagle-im/releases/download/#{version}/BeagleIM.#{version}.zip"
@@ -8,5 +8,5 @@ cask 'beagleim' do
   name 'Tigase BeagleIM'
   homepage 'https://beagle.im/'
 
-  app "BeagleIM #{version}.app"
+  app "BeagleIM.app"
 end
