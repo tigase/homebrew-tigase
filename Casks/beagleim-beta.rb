7aaf786554a3bfb1941767fc9ea4be9c0ea44693
Andrzej Wójcik <andrzej.wojcik@tigase.net>
2020-03-19 18:02:06 +0100

    Upgrade beta to 4.0-b84

--- Casks/beagleim-beta.rb
+++ Casks/beagleim-beta.rb
@@ -1,6 +1,6 @@
 cask 'beagleim-beta' do
-  version '4.0-b83'
-  sha256 'f7a02b2a6fcfd07b10c91b9a43c790f2ac2519a0b5fa0e0e08f0ce792a7ad169'
+  version '4.0-b84'
+  sha256 '7fb2d618c5116ef9f153e083ec0d490cb743a02154188e16a6715409635f1143'
 
   # github.com/tigase/beagle-im was verified as official when first introduced to the cask
   url "https://github.com/tigase/beagle-im/releases/download/#{version}/BeagleIM.#{version}.zip"
