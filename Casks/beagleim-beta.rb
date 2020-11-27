c5b3e2cd489fe493966c591ceb4c6c39359ce370
Andrzej Wójcik <andrzej.wojcik@tigase.net>
2020-11-27 19:35:32 +0100

    Updated beta to 5.0-b111 and added warning

--- Casks/beagleim-beta.rb
+++ Casks/beagleim-beta.rb
@@ -1,6 +1,6 @@
 cask 'beagleim-beta' do
-  version '4.1-b110'
-  sha256 '5f9e378d519ba27e9bd5bf9833ce2f5c9a98bb0da1584bf306a2eba71201c07e'
+  version '5.0-b111'
+  sha256 'd0f8ac97c9371830691af2196d0e56e92171e697c1dc766c6f2d3ddfc89220e2'
 
   # github.com/tigase/beagle-im was verified as official when first introduced to the cask
   url "https://github.com/tigase/beagle-im/releases/download/#{version}/BeagleIM.#{version}.zip"
@@ -11,6 +11,8 @@ cask 'beagleim-beta' do
 
   depends_on macos: ">= :mojave"
 
+  caveats "Beta version may be unstable and contains database schema changes which will make it impossible to rollback to older version. We do suggest to copy `beagleim.sqlite` file using Finder to the safe location before starting the new beta version of BeagleIM."
+  
   app "BeagleIM (beta).app"
 
   zap trash: [
