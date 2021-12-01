cfc17e43ae6420fc5521d1fcd23251d064a91370
Wojtek <woj-tek@users.noreply.github.com>
2021-12-01 12:22:06 -0300

    Update BeagleIM to 5.0

--- Casks/beagleim.rb
+++ Casks/beagleim.rb
@@ -1,6 +1,6 @@
 cask 'beagleim' do
-  version '4.1'
-  sha256 'a59d7bb03fd1b1b201b659760abb0cfc75498a865ac761e00c3f4a03e5598d8d'
+  version '5.0'
+  sha256 '86479aa2676da16a61447b5cf1acf788f4b1578c480184cedae7a99aea916b8a'
 
   # github.com/tigase/beagle-im was verified as official when first introduced to the cask
   url "https://github.com/tigase/beagle-im/releases/download/#{version}/BeagleIM.#{version}.zip"
